14880ed7ff6de1f5e456f1a5aafc78d71519f8cb
Justin Bryan <jb675619@ohio.edu>
2020-05-13 13:45:35 -0400

    created hello git

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Justin Bryan
+ *        Date: 5/13/2020
+ * Description: This program does stuff..?
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
